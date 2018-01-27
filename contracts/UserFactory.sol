fab5f31fb04f8f696f33751ad9d136e2696b2a64
Sean Finney <mcfinneys@gmail.com>
2018-01-27 15:10:29 -0800

    some sort of buddy function

--- contracts/UserFactory.sol
+++ contracts/UserFactory.sol
@@ -32,21 +32,25 @@ contract UserFactory {
     // this is a security thing as we give new users shark coin, and if they sign up a new user, we give them coins
     address u = new User(uid, address(this), name, surname, interests, about);
     users[uid] = Data({
-      uid: uid,
-      factory: address(this),
-      name: name,
-      surname: surname,
-      interests: interests,
-      about: about
-    });
+                        uid: uid,
+                        factory: address(this),
+                        name: name,
+                        surname: surname,
+                        interests: interests,
+                        about: about
+                      });
     usersList.push(uid);
     return u;
   }
 
   // TODO: function to ask if a buddy is registered ot the system
-  /*function buddyRegister(address newBuddy) public
+  function buddyRegister(address newBuddy) public constant returns (bool isBuddy)
   {
-    return false;
-  }*/
+    if (users[newBuddy].uid == ""){
+      return false;
+    } else {
+      return true;
+    }
+  }
   // TODO: Determine how this fits with the ERC223 token
 }
