95a9fa5b2d70edb81eba0e482158ebadfe85cb81
Travis Herrick <quasimonomial@gmail.com>
2018-01-27 12:55:10 -0800

    add some basic functionality to shark coin git st

--- contracts/SharkCoin.sol
+++ contracts/SharkCoin.sol
@@ -1,22 +1,52 @@
-// TODO: Write an ERC 20 token
-// TODO: Add loan functionality
-// TODO: Add "burning coins for reputation" functionality
-// TODO: Check destination of transaction and that it is a user with a user thing
-// TODO: Allow rules to be set for these loans
-
 pragma solidity ^0.4.8;
 
 contract SharkCoin {
-  string public constant name = "Shark Coin";
-
-  string public constant symbol = "SHK";
-
-  uint8 public constant decimals = 0;
+  // TODO: Generate and add supply
   /* function totalSupply() public constant returns (uint);
+   */
 
-  function balanceOf(address tokenOwner) public constant returns (uint balance);
+  mapping(address => uint256) balances;
+  mapping(address => uint256) reputations;
 
-  function transfer(address to, uint tokens) public returns (bool success);
+  string public constant name = "Shark Coin";
+  string public constant symbol = "SHK";
+  uint8 public constant decimals = 0;
 
-  event Transfer(address indexed from, address indexed to, uint tokens); */
+  function balanceOf(address sharkUser) public constant returns (uint balance) {
+    return balances[sharkUser];
+  }
+
+  function transfer(address to, uint tokens) public returns (bool success) {
+    if (balances[msg.sender] < tokens) {
+      Error("You are too poor");
+      return false;
+    }
+
+    balances[msg.sender] = balances[msg.sender].sub(tokens);
+    balances[to] = balances[to].add(tokens);
+    Transfer(msg.sender, to, tokens);
+
+    return true;
+  }
+
+  function reputate(address to, uint tokens) public returns (bool success) {
+    // TODO: we can only send reputation to addresses that have a registered user with our user factory
+    if (balances[msg.sender] < tokens) {
+      Error("You are too poor");
+      return false;
+    }
+
+    balances[msg.sender] = balances[msg.sender].sub(tokens);
+    reputations[to] = reputations[to].add(tokens);
+    Reputate(msg.sender, to, tokens);
+
+    return true;
+  }
+
+  event Error(string msg);
+  event Transfer(address indexed from, address indexed to, uint tokens);
+  event Reputate(address indexed from, address indexed to, uint tokens);
+
+  // TODO: Add loan functionality
+  // TODO: Allow rules to be set for these loans
 }
